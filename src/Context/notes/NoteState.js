30f8d9333a5677b61059aab8b3e19fa48aaac491
Rajib Subedi <subedirajib59@gmail.com>
2025-11-17 21:54:42 +0545

    adding Noteitem in a seperate Note component

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -21,6 +21,78 @@ const notesInitial=[
     "tag": "personal",
     "date": "2025-10-19T15:08:31.918Z",
     "__v": 0
+    },
+    {
+    "_id": "68f4feeff2f6726844815c64",
+    "user": "68f30afd7370fda444dc47ce",
+    "title": "My title",
+    "description": "Hello! My name is Rajib Subedi and I am under water HEHEHEHEHE.....",
+    "tag": "personal",
+    "date": "2025-10-19T15:08:31.918Z",
+    "__v": 0
+    },
+    {
+    "_id": "68f4feeff2f6726844815c64",
+    "user": "68f30afd7370fda444dc47ce",
+    "title": "My title",
+    "description": "Hello! My name is Rajib Subedi and I am under water HEHEHEHEHE.....",
+    "tag": "personal",
+    "date": "2025-10-19T15:08:31.918Z",
+    "__v": 0
+    },
+    {
+    "_id": "68f4feeff2f6726844815c64",
+    "user": "68f30afd7370fda444dc47ce",
+    "title": "My title",
+    "description": "Hello! My name is Rajib Subedi and I am under water HEHEHEHEHE.....",
+    "tag": "personal",
+    "date": "2025-10-19T15:08:31.918Z",
+    "__v": 0
+    },
+    {
+    "_id": "68f4feeff2f6726844815c64",
+    "user": "68f30afd7370fda444dc47ce",
+    "title": "My title",
+    "description": "Hello! My name is Rajib Subedi and I am under water HEHEHEHEHE.....",
+    "tag": "personal",
+    "date": "2025-10-19T15:08:31.918Z",
+    "__v": 0
+    },
+    {
+    "_id": "68f4feeff2f6726844815c64",
+    "user": "68f30afd7370fda444dc47ce",
+    "title": "My title",
+    "description": "Hello! My name is Rajib Subedi and I am under water HEHEHEHEHE.....",
+    "tag": "personal",
+    "date": "2025-10-19T15:08:31.918Z",
+    "__v": 0
+    },
+    {
+    "_id": "68f4feeff2f6726844815c64",
+    "user": "68f30afd7370fda444dc47ce",
+    "title": "My title",
+    "description": "Hello! My name is Rajib Subedi and I am under water HEHEHEHEHE.....",
+    "tag": "personal",
+    "date": "2025-10-19T15:08:31.918Z",
+    "__v": 0
+    },
+    {
+    "_id": "68f4feeff2f6726844815c64",
+    "user": "68f30afd7370fda444dc47ce",
+    "title": "My title",
+    "description": "Hello! My name is Rajib Subedi and I am under water HEHEHEHEHE.....",
+    "tag": "personal",
+    "date": "2025-10-19T15:08:31.918Z",
+    "__v": 0
+    },
+    {
+    "_id": "68f4feeff2f6726844815c64",
+    "user": "68f30afd7370fda444dc47ce",
+    "title": "My title",
+    "description": "Hello! My name is Rajib Subedi and I am under water HEHEHEHEHE.....",
+    "tag": "personal",
+    "date": "2025-10-19T15:08:31.918Z",
+    "__v": 0
     }
 ]
 
